2e8cca226829390c688e363a6c8455e4eca21c8e
iam-vihaan <asathyavihaan@gmail.com>
2025-06-12 14:48:43 +0530

    Create variable.tf

---
+++ modules/IAM/variable.tf
@@ -0,0 +1,28 @@
+# IAM Module Variables
+
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
+
+variable "cluster_oidc_issuer_url" {
+  description = "The URL on the EKS cluster OIDC Issuer"
+  type        = string
+  default     = ""
+}
+
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default     = {}
+}
